ed1ed094b86157a865ed5e7b750b5b418bca7dfc
Justin Braben <brabenj1996@gmail.com>
2024-08-05 19:16:11 -0600

    Update example

--- examples/FallingBlocks/falling_blocks.cpp
+++ examples/FallingBlocks/falling_blocks.cpp
@@ -4,5 +4,19 @@
 int main()
 {
 	std::cout << "Testing falling_blocks\n";
-	return 0;
+
+	Sigil::Configuration engine_config;
+	int return_code = 0;
+
+	// Make some functions to set param name and param value of
+	// Configuration config_json member 
+
+	// engine_config.set("fps", 60)
+    //     .set("display/width", 1024)
+    //     .set("display/height", 768)
+    //     .set("display/fullscreen", true)
+    //     .set("display/resizable", false)
+    //     .set("assets/file/location", SGE_SOURCE_DIR);
+
+	return return_code;
 }
